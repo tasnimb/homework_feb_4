9b79408952ec3576597ec9ca07b3dcd9d12e7259
Tasnim <Tasnim.b98@gmail.com>
2021-02-08 18:22:18 +0000

    new comment by tasnim

--- operations.py
+++ operations.py
@@ -2,3 +2,4 @@ var = input("Please enter a value: ")
 print("upper case:", var.upper(), "length is ", len(var))
 print(var.isdecimal())
 # making sure git pull is working for tasnim
+# another change by tasnim
